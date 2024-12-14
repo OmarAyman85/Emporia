e67e0372214a9236bab484f5cf0c61a438e65385
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-14 18:51:41 +0200

    authentication and authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -40,3 +40,29 @@ export const register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+//------------------------------------------------------------------------------------------------
+//------------------------------ LOGGING IN ------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    delete user.password; // to be not sent to the frontend.
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//------------------------------------------------------------------------------------------------
+//------------------------------ LOGGING IN ------------------------------------------------------
+//------------------------------------------------------------------------------------------------
--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 //------------------------------------------------------------------------------------------------
@@ -54,6 +56,11 @@ const upload = multer({ storage });
 //----------route----------middleware function--registering the picture using the auth controller-
 app.post("auth/register", upload.single("picture"), register);
 
+//------------------------------------------------------------------------------------------------
+//----------------------------------- Routes -----------------------------------------------------
+//------------------------------------------------------------------------------------------------
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 //------------------------------------------------------------------------------------------------
 //------------------------ MONGOOSE SETUP --------------------------------------------------------
 //------------------------------------------------------------------------------------------------
