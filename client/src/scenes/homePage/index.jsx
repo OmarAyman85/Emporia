c4a72e44e18de792a452cbc49c57431e70ee84fb
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-21 15:58:28 +0200

    Introducing the NavBar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,13 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box>
+      <h1>Homepage</h1>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default HomePage;
